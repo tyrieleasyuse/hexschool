a974864f6051dcde23410fab34f0314efdd96d38
陳吉龍 (Tyriel Chen) <tyriel.chen@easyuse.com.tw>
2021-04-22 17:54:52 +0800

    Vue直播班ToDoList

---
+++ Vue直播班/2.新增刪除ToDoList/all.js
@@ -0,0 +1,87 @@
+
+const component = {
+  data : [
+    {'content':'代辦事項1'},
+    {'content':'代辦事項2'},
+    {'content':'代辦事項3'}
+  ],
+  saveData(text){
+    if(text == ''){
+      alert('請輸入代辦事項');
+      return;
+    }
+    let obj ={};
+    obj.content = text;
+    this.data.push(obj);
+    this.renderList();
+  },
+  removeData(index){
+    this.data.splice(index,1);
+    this.renderList();
+  },
+  renderList(){
+    const list = document.querySelector('.component .list');
+    list.innerHTML = '';
+    this.data.forEach((item,index) => {
+      let str = `
+      <li>${item.content} <input type="button" class="delete" data-num="${index}" value="刪除"></li>
+      `;
+      list.innerHTML += str;
+    })
+
+    const deleteBtn = document.querySelectorAll('.component .list .delete');
+    
+    deleteBtn.forEach(item => item.addEventListener('click',(e)=>{
+      console.log(e.target.getAttribute('data-num'));
+      this.removeData(e.target.getAttribute('data-num'));
+    }));
+    
+  },
+  init(){
+    this.renderList();
+    
+
+    const txt = document.querySelector('.txt');
+    const save = document.querySelector('.save');
+    save.addEventListener('click',(e) => {
+      console.log(save);
+      this.saveData(txt.value);
+    });
+  }
+}
+
+component.init();
+
+// const list = document.querySelector('.list');
+
+// function renderList(){
+//   list.innerHTML = '';
+//   data.forEach(function(item,index){
+//     let str = `
+//     <li>${item.content} <input type="button" class="delete" data-num="${index}" value="刪除"></li>
+//     `;
+//     list.innerHTML += str;
+//   })
+// }
+
+// save.addEventListener('click',function(e){
+//   if(txt.value == ''){
+//     alert('請輸入代辦事項');
+//     return;
+//   }
+//   let obj ={};
+//   obj.content = txt.value;
+//   data.push(obj);
+//   renderList();
+// });
+
+// list.addEventListener('click',function(e){
+//   if(e.target.getAttribute('class') != 'delete'){
+//     return;
+//   }
+//   let num = e.target.getAttribute('data-num');
+//   data.splice(num,1);
+//   renderList();
+// })
+
+// renderList();
